f994fd591fcf586bd81fa49abc38e35cfa7260f3
Rehamali87 <omhima354@gmail.com>
2026-09-13 09:13:11 +0200

    Implement file write and read operations
    Add functionality to write and read from 'text.txt'.

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,6 +21,11 @@ MY_QUESTION = "Was Schönes könnte heute passieren?"
 # ==================================================
 
 st.set_page_config(page_title=APP_NAME, page_icon="💡", layout="centered")
+with open("text.txt") as wf:
+    wf.write("Hello world")
+
+with open("text.txt") as rf:
+    rf.read()
 
 COLORS = {
     "purple": "#9b59b6", "blue": "#3498db", "green": "#2ecc71",
