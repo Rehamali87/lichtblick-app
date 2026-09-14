de7c5cb0310cb50578c413a7252140043d5b0228
Rehamali87 <omhima354@gmail.com>
2026-09-14 18:22:48 +0200

    Remove comment on return format in db.py
    Removed comment about returning data as a dictionary.

--- db.py
+++ db.py
@@ -35,8 +35,6 @@ def get_memories(user_key):
     )
     rows = c.fetchall()
     conn.close()
-
-    # WICHTIG: Rückgabe IMMER als Dictionary
     return [{"title": r[0], "text": r[1]} for r in rows]
 
 init_db()
